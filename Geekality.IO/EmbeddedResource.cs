03e0f02150e887c6e4f9c927603fab08b84715ce
Torleif <torleif@geekality.net>
2012-09-21 16:38:08 +0200

    Initial commit.

---
+++ Geekality.IO/EmbeddedResource.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geekality.IO
+{
+    public class EmbeddedResource
+    {
+        /// <summary>
+        /// Get an embedded resource from the currently loaded assembly where type T is specified.
+        /// </summary>
+        /// <typeparam name="T">A type specified in wanted assembly.</typeparam>
+        /// <param name="name">Name of the resource.</param>
+        /// <param name="useNamespaceOfType">If true, the namespace of the type will be prefixed to the name. Defaults to true.</param>
+        /// <returns>Stream with embedded resourc.e</returns>
+        public static Stream Get<T>(string name, bool useNamespaceOfType = true)
+        {
+            var assembly = Assembly.GetAssembly(typeof(T));
+            var s = useNamespaceOfType
+                ? assembly.GetManifestResourceStream(typeof(T), name)
+                : assembly.GetManifestResourceStream(name);
+
+            if (s != null)
+                return s;
+            throw new ArgumentException("Embedded resource not found.");
+        }
+
+
+        /// <summary>
+        /// Get an embedded resource from the calling assembly.
+        /// </summary>
+        /// <typeparam name="T">A type specified in wanted assembly.</typeparam>
+        /// <param name="name">Name of the resource.</param>
+        /// <returns>Stream with embedded resource.</returns>
+        public static Stream Get(string name)
+        {
+            var s = Assembly.GetCallingAssembly().GetManifestResourceStream(name);
+            if (s != null)
+                return s;
+            throw new ArgumentException("Embedded resource not found.");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+Introduction
+===
+
+Some simple IO related helper classes.
+
+- TemporaryFile for easy creation and disposing of temporary files. Especially useful in unit tests.
+- EmbeddedResource for easy access to embedded resources.
+
+
+
+License
+===
+
+This work is licensed under the Creative Commons Attribution 3.0 Unported License. To view a copy of this license, visit [Creative Commons Attribution 3.0 Unported License](http://creativecommons.org/licenses/by/3.0/).
+
+![Creative Commons License](http://i.creativecommons.org/l/by/3.0/88x31.png)
